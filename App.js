4702f2dbc47b2fc8859e2e6a0262e72d15502821
Peter Cheng <petercheng7788@gmail.com>
2019-09-30 01:14:11 +0800

    no message

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step 556</Text>
+              <Text style={styles.sectionTitle}>Step ggg</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
